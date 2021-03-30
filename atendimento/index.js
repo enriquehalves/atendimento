88568affdeb84a9d629f547f286c3df4b8fb15f0
Henrique <enriquehalves@gmail.com>
2021-03-29 23:00:34 -0300

    conclusão do projeto

--- atendimento/index.js
+++ atendimento/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     function camposOcultos() {
-        $(".cpf ,.cnpj").hide()
+        $(".cpfcnpj").hide()
     }
     function botoes() {
         $(".botao").hide()
@@ -14,10 +14,17 @@ $(document).ready(function () {
 
     function data() {
         var data = new Date();
-        var ano = data.getFullYear()
-        var mes = data.getMonth() + 1;
-        var dia = data.getDate();
-        $(".data").val(dia + "/" + mes + "/" + ano)
+        var dia = ("0" + data.getDate()).slice(-2);
+        var mes = ("0" + (data.getMonth() + 1)).slice(-2);
+        var ano4 = data.getFullYear();
+        var hora = ("0" + data.getHours()).slice(-2);
+        var min = ("0" + data.getMinutes()).slice(-2);
+        var str_data = dia + '/' + mes + '/' + ano4;
+        var str_hora = hora + ':' + min;
+
+        console.log(str_data + " " + str_hora);
+
+        $(".data").val(str_data)
 
     }
 
@@ -26,47 +33,41 @@ $(document).ready(function () {
             $("#vainet").hide()
             $("#netturbo").hide()
             $(".botao").show()
-            $(".cpf").show()
-            $(".cnpj").show()
-
-
+            $(".cpfcnpj").show()
         })
         $("#vainet").click(function () {
             $("#dbug").hide()
             $("#netturbo").hide()
             $(".botao").show()
-            $(".cpf").show()
-            $(".cnpj").show()
+            $(".cpfcnpj").show()
+
         })
         $("#netturbo").click(function () {
             $("#vainet").hide()
             $("#dbug").hide()
             $(".botao").show()
-            $(".cpf").show()
-            $(".cnpj").show()
+            $(".cpfcnpj").show()
+
         })
     }
 
     function mascaras() {
 
-         
+        var options = {
+            onKeyPress: function (cpf, ev, el, op) {
+                var masks = ['000.000.000-000', '00.000.000/0000-00'];
+                $('.campocpfcnpj').mask((cpf.length > 14) ? masks[1] : masks[0], op);
+            }
+        }
 
-         
-        /*var campocpf = $(".campocpf")
-        campocpf.mask('000.000.000-00',{reverse:true})
-        var campocnpj = $(".campocnpj")
-        campocnpj.mask("00.000.000/0000-00",{reverse:true})*/
+        $('.campocpfcnpj').length > 11 ? $('.campocpfcnpj').mask('00.000.000/0000-00', options) : $('.campocpfcnpj').mask('000.000.000-00#', options);
 
     }
 
-    function recarregar() {
-        $(".botao").click(function () {
 
-            location.reload()
-        })
-    }
-
-
+    $(".botao").click(function () {
+        location.reload()
+    })
 
 
     camposOcultos()
